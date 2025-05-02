e430a4f4a59b92d55d3815dbe1e4e60dbcefe2b2
AIQuira <166058744+AIQuira@users.noreply.github.com>
2025-05-01 23:39:18 -0500

    Creación del proyecto angular y componente registrar docente

---
+++ FrontEnd/GestionRA-FrontEnd/src/app/DocentesyEvaluadores/registrar-docente/registrar-docente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-docente',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrar-docente.component.html',
+  styleUrl: './registrar-docente.component.css'
+})
+export class RegistrarDocenteComponent {
+
+}
