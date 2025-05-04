4a6287108e28562967cfa1eeb437d987af21d559
ashlee <ashleecampaz@unicauca.edu.co>
2025-05-03 21:43:05 -0500

    añadi los roles al jwt y realice cambios a la configuracion de seguridad

--- BackEnd/servicioLogin/src/main/java/co/unicauca/ra/configuration/JwtService.java
+++ BackEnd/servicioLogin/src/main/java/co/unicauca/ra/configuration/JwtService.java
@@ -8,7 +8,6 @@ package co.unicauca.ra.configuration;
  *
  * @author ashle
  */
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -19,27 +18,38 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.time.Instant;
 import java.util.Base64;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
 
 @Service
 public class JwtService {
 
     private static final String SECRET = "638CBE3A90E0303BF3808F40F95A7F02A24B4B5D029C954CF553F79E9EF1DC0384BE681C249F1223F6B55AA21DC070914834CA22C8DD98E14A872CA010091ACC";
-    private static final long VALIDITY = TimeUnit.MINUTES.toMillis(30);
+    private static final long VALIDITY = TimeUnit.MINUTES.toMillis(5);
 
     public String generateToken(UserDetails userDetails) {
         Map<String, String> claims = new HashMap<>();
+        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+
+        List<String> roles = authorities.stream()
+                .map(GrantedAuthority::getAuthority) // devuelve "ROLE_COORDINADOR", por ejemplo
+                 .map(role -> role.startsWith("ROLE_") ? role.substring(5) : role)
+                .collect(Collectors.toList());
         claims.put("iss", "https://secure.genuinecoder.com");
-         return Jwts.builder()
-            .setClaims(claims)
-            .setSubject(userDetails.getUsername())
-            .setIssuedAt(Date.from(Instant.now()))
-            .setExpiration(Date.from(Instant.now().plusMillis(VALIDITY)))
-            .signWith(generateKey(), SignatureAlgorithm.HS256) 
-            .compact();
+        return Jwts.builder()
+                .setClaims(claims)
+                .claim("roles", roles) 
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plusMillis(VALIDITY)))
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private SecretKey generateKey() {
@@ -54,10 +64,10 @@ public class JwtService {
 
     private Claims getClaims(String jwt) {
         return Jwts.parserBuilder()
-            .setSigningKey(generateKey()) // usar setSigningKey en parserBuilder
-            .build()
-            .parseClaimsJws(jwt)
-            .getBody();
+                .setSigningKey(generateKey()) // usar setSigningKey en parserBuilder
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
     }
 
     public boolean isTokenValid(String jwt) {
--- BackEnd/servicioLogin/src/main/java/co/unicauca/ra/configuration/SecurityConfig.java
+++ BackEnd/servicioLogin/src/main/java/co/unicauca/ra/configuration/SecurityConfig.java
@@ -14,13 +14,12 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  *
@@ -29,38 +28,42 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
+
     @Autowired
     private DocenteUserDetailsService userDetailService;
 
-    
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(); 
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
-    @Bean 
-    public UserDetailsService userDetailsService(){
-        return userDetailService; 
-    }
+
     @Bean
-    public AuthenticationProvider authenticationProvider(){
+    public UserDetailsService userDetailsService() {
+        return userDetailService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailService);
         provider.setPasswordEncoder(passwordEncoder());
-        return provider; 
+        return provider;
     }
+
     @Bean
-    public AuthenticationManager authenticationManager(){
-        return new ProviderManager(authenticationProvider()); 
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(authenticationProvider());
     }
-    
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(registry -> {
-                    registry.requestMatchers("/**").permitAll();
-                })
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/login", "/public/**").permitAll() // rutas públicas
+                .anyRequest().authenticated() // el resto requiere token
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 }
