c6b3cb1c91cfa3c148634224b543359bdef99d96
ashlee <ashleecampaz@unicauca.edu.co>
2025-05-06 21:23:29 -0500

    cambio en la duracion de jwt

--- BackEnd/servicioLogin/src/main/java/co/unicauca/ra/configuration/JwtService.java
+++ BackEnd/servicioLogin/src/main/java/co/unicauca/ra/configuration/JwtService.java
@@ -31,7 +31,7 @@ import org.springframework.security.core.GrantedAuthority;
 public class JwtService {
 
     private static final String SECRET = "638CBE3A90E0303BF3808F40F95A7F02A24B4B5D029C954CF553F79E9EF1DC0384BE681C249F1223F6B55AA21DC070914834CA22C8DD98E14A872CA010091ACC";
-    private static final long VALIDITY = TimeUnit.MINUTES.toMillis(5);
+    private static final long VALIDITY = TimeUnit.MINUTES.toMillis(60);
 
     public String generateToken(UserDetails userDetails) {
         Map<String, String> claims = new HashMap<>();
