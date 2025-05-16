28bbddcf67cd05f6e3b50bf42598a70a21f10616
andres serna andres <wsernam@unicauca.edu.co>
2025-05-16 14:45:06 -0500

    Revert "Se elimina el submódulo y se sube el microservicio como parte del repositorio principal"
    This reverts commit ec2bc6686b5fddfe78591b27fecb3efce358fdeb.
    :wq

--- BackEnd/servicio-ra-asignatura/src/main/java/co/edu/co/servicio_ra_asignatura/repository/CompetenciaAsignaturaRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.co.servicio_ra_asignatura.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import co.edu.co.servicio_ra_asignatura.model.CompetenciaAsignatura;
-import java.util.List;
-
-public interface CompetenciaAsignaturaRepository extends MongoRepository<CompetenciaAsignatura, String> {
-    List<CompetenciaAsignatura> findByAsignaturaId(String asignaturaId);
-}
--- BackEnd/servicio-ra-asignatura/src/main/java/co/edu/co/servicio_ra_asignatura/repository/RAasignaturaRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.co.servicio_ra_asignatura.repository;
-
-import co.edu.co.servicio_ra_asignatura.model.RAasignatura;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface RAasignaturaRepository extends MongoRepository<RAasignatura, String> {
-    List<RAasignatura> findByAsignaturaId(String asignaturaId);
-}
