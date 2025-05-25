4bea1f6fddfe77de0c78c17c269a58f04e1e1adf
wsernam <wsernam@unicauca.edu.co>
2025-05-24 21:34:26 -0500

    feat: agregar endpoint para listar asignaturas

--- BackEnd/servicioAsignaturasCursos/src/main/java/co/unicauca/ra/service/AsignaturaService.java
+++ BackEnd/servicioAsignaturasCursos/src/main/java/co/unicauca/ra/service/AsignaturaService.java
@@ -1,8 +1,14 @@
 package co.unicauca.ra.service;
 
+import co.unicauca.ra.model.Asignatura;
 import co.unicauca.ra.model.AsignaturaCurso;
+import co.unicauca.ra.repository.AsignaturaRepository;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,14 +18,29 @@ public class AsignaturaService {
 
     // Lista temporal para simular persistencia
     private List<AsignaturaCurso> asignaturasCursos = new ArrayList<>();
+    @Autowired
+    private AsignaturaRepository asignaturaRepository;
 
     public ResponseEntity<?> save(AsignaturaCurso asignaturaCurso) {
         asignaturasCursos.add(asignaturaCurso);
         return ResponseEntity.status(HttpStatus.CREATED).body("AsignaturaCurso registrada correctamente.");
     }
 
-    public List<AsignaturaCurso> findAll() {
-        return asignaturasCursos;
+       public List<AsignaturaCurso> findAll() {
+        List<Asignatura> asignaturas = asignaturaRepository.findAll();
+
+        return asignaturas.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    private AsignaturaCurso convertToDTO(Asignatura asignatura) {
+        return new AsignaturaCurso(
+                asignatura.getNombre(),
+                asignatura.getCreditos(),
+                asignatura.getObjetivos(),
+                asignatura.getSemestre()
+        );
     }
 
     public ResponseEntity<?> deleteByNombre(String nombre) {
